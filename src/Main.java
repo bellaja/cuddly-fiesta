dfa828d21250055f655e7827f627ab88b8f0266a
bellaja <bellaja185@gmail.com>
2023-02-15 23:41:39 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
+        int[] salesCount = {4,5,6,7,8,9};
+        int maxNumber = salesCount[0];
+        for (int i = 0; i < salesCount.length; i++) {
+            if (salesCount[i] > maxNumber) {
+                maxNumber = salesCount[i];
+            }
+        }
+        int minNumber = salesCount[0];
+        for (int i = 0; i < salesCount.length; i++) {
+            if (salesCount[i] < minNumber) {
+                minNumber = salesCount[i];
+            }
+        }
 
-        SalesManager manager = new SalesManager(new long[] {4,5,6,7,8,9});
+        SalesManager manager = new SalesManager(salesCount);
 
-        System.out.println(manager.max());
+        int sum = 0;
+        for (int i = 0; i < salesCount.length; i++) {
+            sum = sum + salesCount[i];
+        }
+        sum = sum - minNumber - maxNumber;
+        System.out.println(sum);
 
     }
 }
